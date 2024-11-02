431accf928db70b9ab5dfbb680e9dd59e0e41b4d
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-11-02 20:44:36 +0300

    hw3: reorganize files and new scene

---
+++ mdklychkovPW2/Scenes/WishStoring/WishStoringViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WishStoringViewController.swift
+//  mdklychkovPW2
+//
+//  Created by Maksim Klychkov on 02.11.2024.
+//
+
+import UIKit
+
+final class WishStoringViewController: UIViewController {   
+    override func viewDidLoad() {
+        view.backgroundColor = .magenta
+    }
+}
