ddc60f829ab3c444ad29b7199cf14d6d3442c2a8
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-11-02 21:10:26 +0300

    hw3: wishstoring scene init

--- mdklychkovPW2/Scenes/WishStoring/WishStoringViewController.swift
+++ mdklychkovPW2/Scenes/WishStoring/WishStoringViewController.swift
@@ -7,8 +7,38 @@
 
 import UIKit
 
-final class WishStoringViewController: UIViewController {   
+final class WishStoringViewController: UIViewController {
+    enum Constants {
+        static let tableCornerRadius: Double = 16
+        static let tableOffset: Double = 16
+    }
+    
+    private let table: UITableView = UITableView(frame: .zero)
+    
     override func viewDidLoad() {
         view.backgroundColor = .magenta
+        configureTable()
     }
+    
+    private func configureTable() {
+        view.addSubview(table)
+        table.backgroundColor = .white
+        table.dataSource = self
+        table.separatorStyle = .singleLine
+        table.layer.cornerRadius = Constants.tableCornerRadius
+        table.pin(to: view, Constants.tableOffset)
+    }
+}
+
+
+// MARK: - UITableViewDataSource
+extension WishStoringViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return .init()
+    }
+    
 }
