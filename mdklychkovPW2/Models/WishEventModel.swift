5f74941e232e0d8b14f453a460cde1bc4d602037
Maksim Klychkov <maxim.klychkov64@gmail.com>
2024-12-02 20:05:09 +0300

    hw4: WishEventModel

---
+++ mdklychkovPW2/Models/WishEventModel.swift
@@ -0,0 +1,15 @@
+//
+//  WishEventModel.swift
+//  mdklychkovPW2
+//
+//  Created by Maksim Klychkov on 02.12.2024.
+//
+
+import Foundation
+
+struct WishEventModel {
+    var title: String
+    var description: String?
+    var startDate: Date
+    var endDate: Date
+}
---
+++ mdklychkovPW2/Scenes/WishCalendar/Views/WishEventCell.swift
@@ -0,0 +1,8 @@
+//
+//  WishEventCell.swift
+//  mdklychkovPW2
+//
+//  Created by Maksim Klychkov on 02.12.2024.
+//
+
+import Foundation
